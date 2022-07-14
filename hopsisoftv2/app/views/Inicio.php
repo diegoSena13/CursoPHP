50f7a8541bb2b46058fb9c2f1ae55457df37d625
dr0431095@gmail.com <dr0431095@gmail.com>
2022-07-14 16:11:55 -0500

    ultimo video de walter

--- hopsisoftv2/app/views/Inicio.php
+++ hopsisoftv2/app/views/Inicio.php
@@ -1,39 +1,6 @@
 <?php
 require_once 'inc/header.php';?>;
 
-<div class="container">
-  <div class="row">
-  <div class="col s1">1</div>
-      <div class="col s6">
-      <nav>
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo">Logo</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-    </div>
-  </nav>
-      </div>
-      <div class="col s6">
-      <div class="row">
-    <div class="col s12 m6">
-      <div class="card blue-grey darken-1">
-        <div class="card-content white-text">
-          <span class="card-title">Card Title</span>
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
-        </div>
-        <div class="card-action">
-          <a href="#">This is a link</a>
-          <a href="#">This is a link</a>
-        </div>
-      </div>
-    </div>
-  </div>
-      </div>
-  </div>
-</div>
+<!-- contenido del sitio usando boopstrap5 -->
 
 <?php require_once 'inc/footer.php'; ?>;
